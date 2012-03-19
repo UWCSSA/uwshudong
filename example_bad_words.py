16bb8d2cee387ae170d1d5674f0d4231e295bd8d
Bo Zhu <zhubo03@gmail.com>
2012-03-19 11:46:51 -0400

    added example config files

---
+++ example_bad_words.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+BAD_WORDS_LIST = (
+    u"this_is_really_bad",
+)
+
+if __name__ == '__main__':
+    for w in BAD_WORDS_LIST:
+        print w
---
+++ example_credentials.py
@@ -0,0 +1,11 @@
+USERNAME = 'weibo_username'
+PASSWORD = 'weibo_password'
+
+APP_KEY = 'weibo_app_key'
+
+PRIVATE_KEY = 'reCAPTCHA_priv_key'
+
+AKISMET_API_KEY = 'Akismet_api_key'
+AKISMET_BLOG_URL = 'your_website_url'
+
+TYPEPAD_API_KEY = 'TypePad_api_key'
